896c904d4accdbbd41d43a54c29af802d4ade59c
briiel <144537019+briiel@users.noreply.github.com>
2026-02-03 01:06:48 +0800

    Update artwork.ts
    - Updated Augustin Justinat birth and death year, and fetched the image cover.

--- data/artworks.ts
+++ data/artworks.ts
@@ -4804,7 +4804,7 @@ const ARTWORKS_A_TO_M: Artwork[] = [
     sellingDimensions: "85 x 75",
     price: 8624,
     image: "/image/j/portrait of louis xv.webp",
-    artistLifespan: "Unknown-1743",
+    artistLifespan: "1692-1743",
     slug: "portrait-of-louis-xv",
     letter: "J"
   },
